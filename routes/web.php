5f488a75a8f417bf6b007d4d94bc46eaf5be5cee
Joe L <munir.sirojoel16@gmail.com>
2024-11-14 14:43:55 +0700

    "Deleted multiple files and routes, including controllers, models, migrations, and seeders."

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BookController;
 
 Route::get('/', function () {
     return view('welcome'); // Atau halaman utama aplikasi Anda
 });
-Route::get('/books', [BookController::class, 'index']);
+
