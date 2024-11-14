94792ac24463d7c1a7092d7a91abd41bac0cad48
Joe L <munir.sirojoel16@gmail.com>
2024-11-14 23:01:48 +0700

    "Added validation rules and custom validation replacers to Customer model"

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Validation\Validator;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -9,11 +10,56 @@ class Customer extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name', 'birthdate'];
+    protected $fillable = [
+        'name',
+        'birthdate',
+    ];
 
     // Relasi One-to-Many dengan model Product
     public function products()
     {
         return $this->hasMany(Product::class);
+    }
+
+     /**
+     * Aturan validasi untuk model ini.
+     *
+     * @return array
+     */
+    public static function rules($process)
+    {
+        if ($process == 'insert') {
+            return [
+                'name' => 'required|string|max:225',
+                'birthdate' => 'required|date',
+            ];
+        } elseif ($process == 'update') {
+            return [
+                'name' => 'required|string|max:225',
+                'birthdate' => 'required|date',
+            ];
+        }
+    }
+
+    /**
+     * Mendaftarkan aturan validasi kustom.
+     *
+     * @param  \Illuminate\Validation\Validator  $validator
+     * @return void
+     */
+    public static function customValidation(Validator $validator)
+    {
+        $customAttributes = [
+            'name' => 'Nama',
+            'birthdate' => 'Tanggal Lahir',
+        ];
+
+        $validator->addReplacer('required', function ($message, $attribute, $rule, $parameters) use ($customAttributes) {
+            return str_replace(':attribute', $customAttributes[$attribute], ':attribute harus diisi.');
+        });
+
+        $validator->addReplacer('date', function ($message, $attribute, $rule, $parameters) use ($customAttributes) {
+            return str_replace(':attribute', $customAttributes[$attribute], ':attribute harus berupa tanggal.');
+        });
     }
 }
