0067cad90e353071b63ecf0c5d845260034b42ea
Joe L <munir.sirojoel16@gmail.com>
2024-11-15 16:38:45 +0700

    "Deleted CustomerController.php and updated api.php route to use ProductController instead of ProductController"

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\ProductController;
 
 Route::prefix('v1')->group(function () {
 
